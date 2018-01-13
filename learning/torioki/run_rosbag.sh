af34c055bdad1b8c9200e2ef15b68eb536f86515
Akira Taniguchi <a.taniguchi@em.ci.ritsumei.ac.jp>
2018-01-14 02:19:36 +0900

    file削除をgitに反映

--- learning/torioki/run_rosbag.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-#Akira Taniguchi 2017/02/03-
-#botsu
-
-rosbag play --clock ~/Dropbox/SpCoSLAM/rosbag/albert-b-laser-vision/albert-B-laser-vision-dataset/albertBimgA.bag --pause
-sleep 100
-rosbag play --clock ~/Dropbox/SpCoSLAM/rosbag/albert-b-laser-vision/albert-B-laser-vision-dataset/albertBimgB.bag --pause
